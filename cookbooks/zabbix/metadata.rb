b19da2a73f31daeaa92833f1749e8e726b8b9276
knakayama <knakayama.sh@gmail.com>
2014-09-14 01:45:39 +0900

    update zabbix cookbook

--- cookbooks/zabbix/metadata.rb
+++ cookbooks/zabbix/metadata.rb
@@ -1,6 +1,6 @@
 name             'zabbix'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'knakayama'
+maintainer_email 'knakayama.sh@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures zabbix'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
