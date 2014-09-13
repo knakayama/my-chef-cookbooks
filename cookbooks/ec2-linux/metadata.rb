fa81fd81cd2bb2fef1f4fc1dbb61091b1c930d80
knakayama <knakayama.sh@gmail.com>
2014-09-13 16:32:53 +0900

    shit

--- cookbooks/ec2-linux/metadata.rb
+++ cookbooks/ec2-linux/metadata.rb
@@ -1,7 +1,7 @@
-name             'aws-linux-common'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+name             'ec2-linux'
+maintainer       'knakayama'
+maintainer_email 'knakayama.sh@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures aws-linux-common'
+description      'Installs/Configures ec2-linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
