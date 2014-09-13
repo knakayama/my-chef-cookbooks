f6e3aa7fbd240e376d1b21cc4c752375aa678099
knakayama <knakayama.sh@gmail.com>
2014-09-14 01:43:32 +0900

    update apache24 cookbook

--- cookbooks/apache24/metadata.rb
+++ cookbooks/apache24/metadata.rb
@@ -1,6 +1,6 @@
-name             'apache'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+name             'apache24'
+maintainer       'knakayama'
+maintainer_email 'knakayama.sh@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
