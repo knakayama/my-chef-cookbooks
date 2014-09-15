3d32d67a0ebd34028b5f89188c104b2fa1ba7925
knakayama <knakayama.sh@gmail.com>
2014-09-15 21:58:16 +0900

    add zabbix-agent cookbook

---
+++ cookbooks/zabbix-agent/metadata.rb
@@ -0,0 +1,7 @@
+name             'zabbix-agent'
+maintainer       'knakayama'
+maintainer_email 'knakayama.sh@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures zabbix-agent'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
